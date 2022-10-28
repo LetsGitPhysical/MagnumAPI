83d9dc69186b906704920ba9e6407b6b01ed0eb6
podasin <podasin@gmail.com>
2022-10-28 10:52:53 -0400

    i will keep pushing to finish prior challenges as quickly as i can

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,56 @@
 
+$(document).ready(function() {
 var schedContainer = $('.container');
-
-const d = new Date();
-var dateStr = String(d);
-var dateArr = dateStr.split(' ');
-console.log(dateArr);  //  [ 'Mon', 'Oct', '17', '2022','02:06:54', 'GMT-0400','(Eastern', 'Daylight', 'Time)' ]
-var dailyDate = `${dateArr[0]} ${dateArr[1]} ${dateArr[2]}, ${dateArr[3]}`;
-var dateHdr = document.getElementById('date-curr');
-dateHdr.textContent = dailyDate;
-
-function addToLocal(event) {
+function addToLocal() {
     var schedTxt = $(this).siblings('.theSched').val();
     var timeTxt = $(this).siblings('.theTime').attr('id');
-    window.localStorage.setItem(timeTxt,schedTxt);
-    // console.log('vals i need time and text: ',timeTxt,schedTxt);
-  }
-
+    // if(schedTxt){
+    localStorage.setItem(timeTxt,schedTxt);
+    $(this).siblings('.theSched').attr('confirm-sched',"added");
+    setTimeout(function() {
+      $(this).siblings('.theSched').attr('confirm-sched',"");
+    }, 2500);
+  // }
+};
 schedContainer.on('click', '.hr-add', addToLocal);
 
-// loop localStorage keys/values 
-for (let [k,v] of Object.entries(localStorage)){
-    $(`.${k} .theSched`).val(v); // BOOOM - needed the '.' before the ${k}!!!!
-    // console.log(k,v);
+
+function clearStoredEvents() {
+  localStorage.clear();
+  updateSched();
 }
+$('#reset-sched').click(clearStoredEvents);
 
 function updateSched() {
-    // get current number of hours
-    var dateNew = new Date();
-    var dateNewStr = String(dateNew);
-    var newHr = dateNewStr.split(' ')[4];
-    var timeArr = newHr.split(':');
-    var currHr = Number(timeArr[0]);
-    // console.log(currHr);
-
-    // loop over time blocks
+    const dateNew = new Date();
+    var dateStr = String(dateNew);
+    var dateArr = dateStr.split(' ');
+    console.log(dateArr);  //  [ 'Mon', 'Oct', '17', '2022','02:06:54', 'GMT-0400','(Eastern', 'Daylight', 'Time)' ]
+    var dailyDate = `${dateArr[0]} ${dateArr[1]} ${dateArr[2]}, ${dateArr[3]}`;
+    $('#date-curr').text(dailyDate);
+    var newHr = dateArr[4].split(':');
+    var currHr = Number(newHr[0]);
+    // console.log('the curr hour: ',currHr); // the current time 
     $('.hr-block').each(function() {
-      var hourSched = Number(parseInt($(this).attr('class').split('-')[2]));
-    //   console.log(hourSched);  // -- boom!!
-
-    //   // check if we've moved past this time
-      if (hourSched < currHr) {
-        var theTimeVal = $(this).children('.theTime');
-        var theSchedVal = $(this).children('.theSched');
-        console.log(theSchedVal.val());
+      var elemHr = Number(parseInt($(this).attr('class').split('-')[2]));
+      var elemID = $(this).children().attr('id');
+      var hourEvent = '';
+      if(localStorage.getItem(elemID)){
+        hourEvent = localStorage.getItem(elemID);
       }
- 
-    })}
-
+      // console.log('elemHr: ',elemHr, elemID, hourEvent);  // -- boom!!
+      $(this).children('.theSched').val(localStorage.getItem(elemID));
+      if(currHr > elemHr){
+        $(this).attr('timecolor','past');
+      }else if(currHr === elemHr){
+        $(this).attr('timecolor','present');
+      }else{
+        $(this).attr('timecolor','future');
+      }
+    });
+  }// close of udpate function
   updateSched();
+  // var runTimer = setInterval(updateSched, 30000); // every 30 seconds updates
+});
+
+
